7b01235b7f70ebbfc3dec1ea1eb5c03e5464aa63
suhaibinsari <mstar390@gmail.com>
2022-04-18 05:26:38 +0500

    new-update

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
+import './Students.css'
+
 
 export default function students() {
+
+ 
+  
   return (
-    <div>students</div>
+    <div>
+      <div className="listStyle">
+<h1>List-Of-Students</h1>
+</div>
+<div className="tableContent">
+
+<table>
+<tr>
+  <th>#</th>
+  <th>Student Name</th>
+  <th>Batch No</th>
+  <th>Roll Number</th>
+</tr>
+<tr>
+  <td>1</td>
+  <td>Suhaib</td>
+  <td>c-2</td>
+  <td>1</td>
+</tr>
+
+
+</table>
+
+</div>
+
+
+    </div>
   )
 }
