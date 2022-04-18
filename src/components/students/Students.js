6907d9a35c8f50a5db5b2747d8576cc23df45ffa
suhaibinsari <mstar390@gmail.com>
2022-04-18 05:56:06 +0500

    new

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -3,35 +3,72 @@ import { useState } from 'react'
 import './Students.css'
 
 
-export default function students() {
+export default function Students() {
 
- 
-  
+  const [students, setStudents] = useState([
+    {
+      name: "Suhaib Ansari",
+      BtNo: "987",
+      RollNo: "01"
+
+    },
+    {
+      name: "Moon",
+      BtNo: "997",
+      RollNo: "21"
+
+    },
+    {
+      name: "Julia",
+      BtNo: "07",
+      RollNo: "61"
+
+    }
+
+
+
+  ])
+
+  console.log("students", Students)
+
   return (
     <div>
       <div className="listStyle">
-<h1>List-Of-Students</h1>
-</div>
-<div className="tableContent">
+        <h1>List-Of-Students</h1>
+      </div>
+      <div className="tableContent">
 
-<table>
-<tr>
-  <th>#</th>
-  <th>Student Name</th>
-  <th>Batch No</th>
-  <th>Roll Number</th>
-</tr>
-<tr>
-  <td>1</td>
-  <td>Suhaib</td>
-  <td>c-2</td>
-  <td>1</td>
-</tr>
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Student Name</th>
+            <th>Batch No</th>
+            <th>Roll Number</th>
+          </tr>
 
 
-</table>
+{
+  students.map((students, index)=>{
+return(
 
-</div>
+  <tr>
+            <td>{index}</td>
+            <td>{students.name}</td>
+            <td>{students.BtNo}</td>
+            <td>{students.RollNo}</td>
+          </tr>
+
+
+)
+  }
+  )
+}
+
+        
+
+        </table>
+
+      </div>
 
 
     </div>
